c211de8e35049c62207824accb5801a960882860
BitPutterIn <matthewharrison@oakland.edu>
2017-08-10 17:52:02 -0400

    Added strings for display in player text views.
    initialized a textView currentPlayer.
    Added method (setCurentPlayer(char) )and instance variable (mark) for setting the current player as well as changing the text displayed in the currentPlayer textView.
    Added method setWinner(char) for setting the currentPlayer TextView to a win message.
    Added a method placeMarkOnBoard(int row, int col) for placing a mark on the board
    
    Fixed:
    Buttons 2-9 setting the listener under them. They also need to be final if they are not instanced variables. They should be instance variables.
    
    Ext:
    Added an example that is commented out called buttonEXMP. This can be used as a template to create other buttons.

--- app/src/main/java/com/example/maste/cse337pro/MainActivity.java
+++ app/src/main/java/com/example/maste/cse337pro/MainActivity.java
@@ -3,25 +3,56 @@ package com.example.maste.cse337pro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 //this is only for display team
+
+    TextView currentPlayer;
+    private char mark;
+    //TODO the buttons should be stored as instance variables as they will need to be
+    //public Button buttonEXMP;
+    //TODO manipulated by other classes like winOrDraw
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //example button
+        //TODO use button.setText('-'); to set the text to a dash this can also be left blank
+        /*buttonEXMP=(Button) findViewById(R.id.button1);
+        buttonEXMP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonEXMP.setText(mark+"");
+                //TODO call placeMarkOnBoard(row,col); to set the mark
+                placeMarkOnBoard( 0 , 0);
+                //Disable the buttone after cliacked
+                buttonEXMP.setEnabled(false);
+               //can make a method
+               if (Character.toUpperCase(mark) == 'X' ){
+                   setCurentPlayer('O');
+               }else{
+                   setCurentPlayer('X');
+               }
+               //TODO checkWinOrDraw in this method call
 
-        Button button1=(Button) findViewById(R.id.button1);
+
+            }
+        });*/
+
+        
+        final Button button1=(Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
-        Button button2=(Button) findViewById(R.id.button2);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button2=(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -30,16 +61,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        Button button3=(Button) findViewById(R.id.button3);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button3=(Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        Button button4=(Button) findViewById(R.id.button4);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button4=(Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -47,45 +78,72 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button button5=(Button) findViewById(R.id.button5);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button5=(Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        Button button6=(Button) findViewById(R.id.button6);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button6=(Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        Button button7=(Button) findViewById(R.id.button7);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button7=(Button) findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        Button button8=(Button) findViewById(R.id.button8);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button8=(Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        Button button9=(Button) findViewById(R.id.button9);
-        button1.setOnClickListener(new View.OnClickListener() {
+        final Button button9=(Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
+
+        //sets up the current player text view use setCurrentPlayer to change player with X or O
+        currentPlayer = (TextView) findViewById(R.id.currentPlayer);
+        //TODO set the current player
 
     }
+
+
+    //sets the current player pass in x or o
+    //it does not matter it will capitalize it for you
+    public void setCurentPlayer(char mark){
+
+        this.mark = Character.toUpperCase(mark);
+        currentPlayer.setText(R.string.current_player_message+mark);
+
+    }
+
+    //displays the current win message in the currentPlayer text view
+    public void setWinner(char winner){
+        currentPlayer.setText(Character.toUpperCase(winner) +R.string.win_message);
+    }
+
+    // row column 0-2 for the set mark use this to call the middleware
+
+    public void placeMarkOnBoard(int rowNumber, int columnNumber){
+        //TODO add in the middleware and database class to the project.
+        //middleware.placeMarkOnBoard(Character.toUpperCase(this.mark),rowNumber ,columnNumber);
+    }
+
 }
